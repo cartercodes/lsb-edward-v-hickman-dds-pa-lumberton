d9c459c8ae7241f5b6fde6be122d15e51ac17b41
Dispatch <dispatch@cartercodes.dev>
2026-04-08 20:59:37 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-edward-v-hickman-dds-pa-lumberton',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
